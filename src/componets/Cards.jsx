b7d4e7ea63836e37a04be635264711ef6284ffbb
Guillermo <guillenicolas99@gmail.com>
2024-10-23 17:06:41 -0600

    fix card responsive

--- src/componets/Cards.jsx
+++ src/componets/Cards.jsx
@@ -1,20 +1,19 @@
 import { Card } from "flowbite-react";
-import { IconBadge4k } from '@tabler/icons-react'
 
 export default function Cards({ current, location }) {
 
     return (
-        <Card className="w-max bg-transparent border-transparent text-white shadow-lg bg-blue-950 hover:scale-105 transition-all">
+        <Card className="w-auto bg-transparent border-transparent text-white shadow-lg bg-blue-950 hover:scale-105 transition-all">
             <article className="flex flex-col space-y-5">
-                <header>
+                <header className="font-sans flex flex-col">
                     <p><strong>El tiempo actual</strong></p>
                     <small>{location.localtime}</small>
                 </header>
-                <div className="flex items-center space-x-3">
-                    <img src={current.condition.icon} alt={current.condition.text} />
+                <div className="flex flex-col space-x-5 md:flex-row">
+                    <img src={current.condition.icon} alt={current.condition.text} width={64} />
+                    <h2 className='text-5xl'>{current.temp_c}&deg;</h2>
                     <div className=' flex space-x-2 items-center'>
-                        <h2 className='text-5xl'>{current.temp_c}&deg;</h2>
-                        <div>
+                        <div className="flex flex-col justify-start">
                             <p>{current.condition.text}</p>
                             <small>Sensación térmica: {current.feelslike_c}&deg;</small>
                         </div>
@@ -23,11 +22,10 @@ export default function Cards({ current, location }) {
                 <h5 className="text-2xl font-bold ">
                     {location.name}, {location.region}, {location.country} ({location.tz_id})
                 </h5>
-                <footer className="flex flex-wrap justify-start space-x-10">
+                <footer className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:flex md:justify-start md:space-x-10">
                     <div className="flex flex-col">
                         <small>
                             Viento
-                            <IconBadge4k stroke={2} />
                         </small>
                         <p>{current.wind_kph} km/h</p>
                     </div>
